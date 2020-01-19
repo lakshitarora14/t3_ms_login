f178eee3c4778137978876a0cfd9aa295cb031d2
lakshitarora14 <lakshitarora14@gmail.com>
2020-01-19 14:05:04 +0530

    first_commit

---
+++ src/test/java/com/coviam/t3login/T3loginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coviam.t3login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T3loginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
