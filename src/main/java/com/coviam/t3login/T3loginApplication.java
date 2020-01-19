03776ad792d0385a893c64d8e6eeefbcbecc7b30
lakshitarora14 <lakshitarora14@gmail.com>
2020-01-19 14:00:55 +0530

    first_commit

---
+++ src/main/java/com/coviam/t3login/T3loginApplication.java
@@ -0,0 +1,13 @@
+package com.coviam.t3login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T3loginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T3loginApplication.class, args);
+	}
+
+}
